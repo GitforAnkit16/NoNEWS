193321261a3600b56ad45eb099a57c8c0edc5961
GitforAnkit16 <111048455+GitforAnkit16@users.noreply.github.com>
2024-05-13 09:37:50 +0530

    Delete config/keys.js

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGOURI: "mongodb+srv://ankitpanda1612:JY4Ez4ZpSGP0S4HF@cluster0.1owlvj7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    JWT_SECRET: "aldiajdfnamnslallahdnsfin"
-}
